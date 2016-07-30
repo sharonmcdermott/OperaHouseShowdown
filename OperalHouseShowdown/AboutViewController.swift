527e0b693195a9d0bbfe01f08c3fc7e38a2733e0
Sharon's Mac <help@pencilevent.com>
2016-07-30 13:11:35 -0400

    Updated graphics with graphics that I created that do not require credits.

--- OperalHouseShowdown/AboutViewController.swift
+++ OperalHouseShowdown/AboutViewController.swift
@@ -10,31 +10,8 @@ import UIKit
 
 class AboutViewController: UIViewController {
     
-
     @IBOutlet weak var textView: UITextView!
     
-    
-//    i = [String]
-//    i = "Graphic Images Credits"
-//    
-//    "stairs with red carpet
-//    <a href= http://www.freepik.com/free-vector/stairs-with-a-red-carpet_797293.htm >Designed by Freepik</a>
-//    Designed by Freepik"
-//    
-//    "Nice boy singing in the bathroom
-//    <a href='http://www.freepik.com/free-vector/nice-boy-singing-in-the-bathroom_846831.htm'>Designed by Freepik</a>"
-//    
-//    "kids making art in flat design
-//    <a href='http://www.freepik.com/free-vector/kids-making-art-in-flat-design_848534.htm'>Designed by Freepik</a>"
-//    
-//    "barbers singing
-//    <a href='http://www.freepik.com/free-vector/barbers-singing_796534.htm'>Designed by Freepik</a>"
-//    
-//    "children artists set
-//    <a href='http://www.freepik.com/free-vector/children-artists-set_847457.htm'>Designed by Freepik</a>"
-//
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -53,16 +30,6 @@ class AboutViewController: UIViewController {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
